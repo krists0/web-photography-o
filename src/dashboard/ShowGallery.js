a3d5c553851204a8f4b99498188a0ef742736b1e
krists0 <christina0147@gmail.com>
2021-06-14 14:05:46 +0300

    com images

--- src/dashboard/ShowGallery.js
+++ src/dashboard/ShowGallery.js
@@ -10,7 +10,7 @@ class ShowGallery extends Component {
  
     return (
       <div className="Gallery">
-        <Glitch></Glitch>
+       
         <Gallery photos={photos}  />
        
       </div>
